4092a0f2a482d6d609d8fa6171e4ee2df6189e65
Tortitas <victorgf2011@gmail.com>
2023-12-05 12:29:11 +0100

    feat: day 1 done

--- 1/app/bin/main.ml
+++ 1/app/bin/main.ml
@@ -3,6 +3,21 @@ let () =
   let lines = In_channel.input_all reader in
   let lines = String.split_on_char '\n' lines in
   let lines = List.filter (fun x -> x <> "") lines in
-  List.iter (fun x -> 
-    x ^ " " ^ (String.length x |> string_of_int) |> print_endline
-  ) lines
+  let numbers = List.map (fun x -> 
+    x 
+    |> String.to_seq 
+    |> List.of_seq 
+    |> List.map (fun x -> Char.escaped x) 
+    |> List.map int_of_string_opt
+    |> List.filter (fun x -> x <> None)
+    |> List.map Option.get
+    |> List.map string_of_int
+    |> List.fold_left (fun acc x -> 
+      match acc with
+      | None -> Some x
+      | Some y -> Some (y ^ x)
+    ) None
+    |> Option.get
+  ) lines in
+  let sum = List.fold_left (fun acc x -> acc + (int_of_string x)) 0 numbers in
+  print_endline (string_of_int sum)
