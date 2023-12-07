3dbdf9d889ec273c12549760239c3af9cc9fe10c
Tortitas <victorgf2011@gmail.com>
2023-12-07 14:39:32 +0100

    feat: day 1 complete

--- 1/app/bin/main.ml
+++ 1/app/bin/main.ml
@@ -4,20 +4,20 @@ let () =
   let lines = String.split_on_char '\n' lines in
   let lines = List.filter (fun x -> x <> "") lines in
   let numbers = List.map (fun x -> 
-    x 
+    let ints = x 
     |> String.to_seq 
     |> List.of_seq 
     |> List.map (fun x -> Char.escaped x) 
     |> List.map int_of_string_opt
     |> List.filter (fun x -> x <> None)
     |> List.map Option.get
-    |> List.map string_of_int
-    |> List.fold_left (fun acc x -> 
-      match acc with
-      | None -> Some x
-      | Some y -> Some (y ^ x)
-    ) None
-    |> Option.get
+    |> List.map string_of_int in
+
+    let first = List.hd ints in
+    let last = List.nth ints ((List.length ints) - 1) in
+
+    first ^ last
   ) lines in
-  let sum = List.fold_left (fun acc x -> acc + (int_of_string x)) 0 numbers in
+
+  let sum = List.fold_left (fun acc x -> acc + int_of_string x) 0 numbers in
   print_endline (string_of_int sum)
